554a5ccc2f56aed66df94038644dac01942d01cd
Mr.ElAlaoui <hraf23@gmail.com>
2024-10-14 23:37:50 +0100

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,6 @@ T = TypeVar('T')
 Res = Union[Any, T]
 Def = Union[T, None]
 
-
 def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     """
     This function gets the value for a given key in a mapping, or returns
